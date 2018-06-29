42401ae4419037c380bcc591191e0945c8c43dfd
teripanda <teri.london@gmail.com>
2018-06-29 15:24:08 -0700

    backend stuffs

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,14 @@
+const app = express(),
+      express = require('express'),
+      http = require('http'),
+      bodyparser = require('bodyparser'),
+      cors = require('cors');
+      mongoose = require('mongoose'),
+      router = require('router'),
+
 const User = require('./user');
 const City = require('./city');
 const Post = require('./post');
-
 
 
 exports.User = User;
--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,19 @@
-const mongoose = require('mongoose');
+const app = express(),
+      express = require('express'),
+      http = require('http'),
+      bodyparser = require('bodyparser'),
+      cors = require('cors');
+      mongoose = require('mongoose'),
+      router = require('router');
+
+const db = require(`./models`);
+
 mongoose.connect('mongodb://localhost:27017');
+
+app.use(cors());
+app.use(bodyparser.json({type: '*/*'}));
+
+router(app);
+const server = http.createServer(app);
+server.listen(process.env.PORT || 3000);
+console.log('Server be listenin on', port);
